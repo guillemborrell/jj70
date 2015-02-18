ae8e48704c63f4a5d5b0869a3390f2f6b38fcd82
Guillem Borrell <guillemborrell@gmail.com>
2015-02-18 12:05:25 +0100

    wsgi app for abstract submission, and conference registration.

---
+++ python/main.py
@@ -0,0 +1,165 @@
+import webapp2
+import jinja2
+import os
+import gdbm
+import json
+import random
+import string
+from webapp2_extras import sessions
+
+LATEX_TEMPLATE = """
+
+"""
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+database = gdbm.open('database.gdbm','cs',0o0660)
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        if self.request.get('error'):
+            if self.request.get('error') == 'userexists':
+                template_values['error'] = 'This user is already registered'
+            if self.request.get('error') == 'security':
+                template_values['error'] = 'Wrong answer to the security question'
+
+        template = JINJA_ENVIRONMENT.get_template('login.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        if ('usr'+username).encode('ascii','ignore') in database:
+            auth_info = json.loads(
+                database[('usr'+username).encode('ascii','ignore')]
+            )
+            if auth_info['password'] == password:
+                session_store = sessions.get_store(request=self.request)
+                session = session_store.get_session()
+                session['key'] = auth_info['key']
+                session['username'] = username
+                session_store.save_sessions(self.response)
+                self.redirect('/jj70/abstracts')
+
+            else:
+                self.redirect('/jj70/login')
+            
+        else:
+            self.redirect('/jj70/login')
+
+        
+class SignupHandler(webapp2.RequestHandler):
+    def post(self):
+        username = self.request.get('username')
+        email = self.request.get('email')
+        firstname = self.request.get('firstname')
+        lastname = self.request.get('lastname')
+        password = self.request.get('password')
+        safety = self.request.get('icode')
+
+        if safety=='Salamanca' or safety=='salamanca':
+            if 'usr'+username.encode('ascii','ignore') in database:
+                self.redirect('/jj70/login?error=userexists')
+
+            else:
+                randstring = ''.join(
+                    [random.choice(
+                        string.ascii_letters + string.digits
+                    ) for n in xrange(32)])
+                auth_info = {'password':password,
+                             'email':email,
+                             'firstname':firstname,
+                             'lastname':lastname,
+                             'key':randstring}
+                
+                database[('usr'+username).encode('ascii','ignore')] = json.dumps(auth_info)
+                session_store = sessions.get_store(request=self.request)
+                session = session_store.get_session()
+                session['key'] = randstring
+                session['username'] = username
+                session_store.save_sessions(self.response)
+
+                self.redirect('/jj70/abstracts')
+
+        else:
+            self.redirect('/jj70/login?error=security')
+            
+
+class AbstractsHandler(webapp2.RequestHandler):
+    def get(self):
+        session_store = sessions.get_store(request=self.request)
+        session = session_store.get_session()
+        username = session.get('username')
+        key = session.get('key')
+
+        if ('usr'+username).encode('ascii','ignore') in database:
+            auth_info = json.loads(
+                database[('usr'+username).encode('ascii','ignore')]
+            )
+
+            if auth_info['key'] == key:
+                template_values = {'auth':auth_info}
+
+                if 'abstract'+key.encode('ascii','ignore') in database:
+                    abstract_info = json.loads(
+                        database['abstract'+key.encode('ascii','ignore')]
+                    )
+                    abstract_info['text'] = abstract_info['text'].replace('\n','<br/>')
+                    template_values['abstract_info'] = abstract_info
+
+                template = JINJA_ENVIRONMENT.get_template('abstracts.html')
+                self.response.write(template.render(template_values))
+                
+
+            else:
+                self.redirect('/jj70/login')
+                
+        else:
+            self.redirect('/jj70/login')
+
+
+    def post(self):
+        self.redirect('/jj70/abstracts')
+            
+
+class AbstractResource(webapp2.RequestHandler):
+    def get(self):
+        session_store = sessions.get_store(request=self.request)
+        session = session_store.get_session()
+        username = session.get('username')
+        key = session.get('key')
+        if 'abstract'+key.encode('ascii','ignore') in database:
+            abstract = database['abstract'+key.encode('ascii','ignore')]
+            self.response.out.headers['Content-Type'] = 'application/json'
+            self.response.out.write(abstract)
+
+        else:
+            self.abort(404)
+        
+    def post(self):
+        session_store = sessions.get_store(request=self.request)
+        session = session_store.get_session()
+        username = session.get('username')
+        key = session.get('key')
+        database['abstract'+key.encode('ascii','ignore')] = self.request.body
+
+
+routes = [
+    (r'/jj70/login', LoginHandler),
+    (r'/jj70/abstracts', AbstractsHandler),
+    (r'/jj70/signup', SignupHandler),
+    (r'/jj70/abstractresource', AbstractResource),
+]
+
+
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'WEReBjJZn8BHmQzKyFwci5A9fOzEWV16',
+}
+
+application = webapp2.WSGIApplication(routes=routes, debug=True, config=config)
+
